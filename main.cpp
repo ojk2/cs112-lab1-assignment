a259dc6ec83b27d9e26b0c7b81651a84d7bbe97b
ojk2 <ojk2@calvin.edu>
2023-01-17 10:52:49 -0500

    work 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
+    char user_letter;
+    cout << "Enter a single lowercase letter: " << endl;
+    cin >> user_letter;
+
+    switch(user_letter) {
+        case 'a':
+            cout << "The letter is a vowel." << endl;
+            break;
+        case 'e':
+            cout << "The letter is a vowel." << endl;
+            break;
+        case 'i':
+            cout << "The letter is a vowel." << endl;
+            break;
+        case 'o':
+            cout << "The letter is a vowel." << endl;
+            break;
+        case 'u':
+            cout << "The letter is a vowel." << endl;
+            break;
+        default:
+            cout << "The letter is a consonant." << endl;
+    }
 
     return 0;
 }
